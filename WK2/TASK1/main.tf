54c1fd86e027eebee53ebf327d357a55248e409b
fikayodir <fikayo.oluwakeye@directwest.com>
2024-06-12 11:39:37 -0600

    Update ec2

--- WK2/TASK1/main.tf
+++ WK2/TASK1/main.tf
@@ -10,8 +10,38 @@ terraform {
 }
 
 
-resource "aws_ssm_parameter" "foo" {
-  name = "foo"
-  type = "String"
-  value = "bar"
+resource "aws_instance" "Vm" {
+  ami = "ami-0bb84b8ffd87024d8"
+  instance_type = "t3.medium"
+  key_name = "FikayoPair"
+  user_data = <<-EOF
+              #!/bin/bash
+              yum update -y
+              yum install -y httpd
+              systemctl start httpd
+              systemctl enable httpd
+              wget https://github.com/prometheus/prometheus/releases/download/v2.52.0/prometheus-2.52.0.linux-amd64.tar.gz
+              tar xvfv prometheus*.tar.gz
+              sudo mv prometheus-2.52.0.linux-amd64 /opt/prometheus
+              EOF
+  network_interface {
+    network_interface_id = aws_network_interface.ENI.id
+    device_index = 0
+  }
+  tags = {
+    Name = "My_ec2"
+  }
+}
+
+resource "aws_network_interface" "ENI" {
+  subnet_id = aws_subnet.Public_subnet.id
+  private_ip = aws_eip.Elastic_ip.public_ip
+  security_groups = [aws_security_group.Faks_Sg.id]
+    tags = {
+      Name = "Public_ec2_eni"
+    }
+}
+
+resource "aws_eip" "Elastic_ip" {
+  domain = "vpc"
 }

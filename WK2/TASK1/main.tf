208b51dfdfbe63530da82128348fba5d5082544f
fikayodir <fikayo.oluwakeye@directwest.com>
2024-06-12 13:07:15 -0600

    Update sg

--- WK2/TASK1/main.tf
+++ WK2/TASK1/main.tf
@@ -56,6 +56,42 @@ resource "aws_security_group" "Faks_Sg" {
   
 }
 
+resource "aws_vpc_security_group_ingress_rule" "allow_http" {
+  security_group_id = aws_security_group.Faks_Sg.id
+  cidr_ipv4 = "0.0.0.0/0"
+  from_port = 80
+  ip_protocol = "tcp"
+  to_port = 80
+  
+}
+
+resource "aws_vpc_security_group_ingress_rule" "allow_ssh" {
+  security_group_id = aws_security_group.Faks_Sg.id
+  cidr_ipv4 = "0.0.0.0/0"
+  from_port = 22
+  ip_protocol = "tcp"
+  to_port = 22
+  
+}
+
+resource "aws_vpc_security_group_ingress_rule" "allow_Icmp" {
+  security_group_id = aws_security_group.Faks_Sg.id
+  cidr_ipv4 = "0.0.0.0/0"
+  from_port = -1
+  ip_protocol = "icmp"
+  to_port = -1
+  
+}
+
+
+resource "aws_vpc_security_group_egress_rule" "Send_http" {
+  cidr_ipv4 = "0.0.0.0/0"
+ // from_port = 80
+  ip_protocol = "-1"
+  //to_port = 80
+  security_group_id = aws_security_group.Faks_Sg.id
+  
+}
 resource "aws_instance" "Vm" {
   ami = "ami-0bb84b8ffd87024d8"
   instance_type = "t3.medium"
